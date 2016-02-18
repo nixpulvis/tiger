821680f5f9f29383b5d2045221841cfe71ee5a67
Nathan Lilienthal <nathan@nixpulvis.com>
2016-02-18 13:40:52 -0500

    Move things into syntax, and use my fork of lalrpop.

--- src/ast/typ.rs
+++
@@ -1,8 +0,0 @@
-use super::Symbol;
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum Type {
-	Name(Symbol),
-	Record(Vec<(Symbol, Symbol)>),
-	Array(Symbol),
-}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,27 @@
-pub mod ast;
-pub mod tiger;
-
-fn compile(source: &str) -> Box<ast::Expression> {
-    match tiger::parse_Expression(source) {
-        Ok(ast) => ast,
-        Err(e) => {
-            println!("{:?}", e);
-            Box::new(ast::Expression::Nil)
-        }
-    }
-}
+pub mod syntax;
 
 fn main() {
-    println!("{:?}", compile("nil"));
-    println!("{:?}", compile("break"));
-    println!("{:?}", compile("123"));
-    println!("{:?}", compile(r#""hello""#));
-    println!("{:?}", compile("foo"));
-    println!("{:?}", compile("foo.bar"));
-    println!("{:?}", compile("foo[(1;2)]"));
-    println!("{:?}", compile("foo.bar[(1;2)]"));
-    println!("{:?}", compile("if 1 then 2"));
-    println!("{:?}", compile("()"));
-    println!("{:?}", compile("(1)"));
-    println!("{:?}", compile("(1; 2)"));
-    println!("{:?}", compile("foo(1, 2)"));
-    println!("{:?}", compile("posn { x=1, y=2 }"));
-    println!("{:?}", compile("foo := nil"));
-    println!("{:?}", compile("while 1 do if 2 then break"));
-    println!("{:?}", compile("let type a = int in 2 end"));
-    println!("{:?}", compile("let var a := 1 in a end"));
-    println!("{:?}", compile("let var a : int := 1 in a end"));
-    println!("{:?}", compile("let function foo(a:int) = () in a end"));
-    println!("{:?}", compile("let function foo(a:int) : int = 1 in a end"));
-    println!("{:?}", compile("foo #5# of 0"));
-    println!("{:?}", compile("-5"));
+    println!("{:?}", syntax::compile("nil"));
+    println!("{:?}", syntax::compile("break"));
+    println!("{:?}", syntax::compile("123"));
+    println!("{:?}", syntax::compile(r#""hello""#));
+    println!("{:?}", syntax::compile("foo"));
+    println!("{:?}", syntax::compile("foo.bar"));
+    println!("{:?}", syntax::compile("foo[(1;2)]"));
+    println!("{:?}", syntax::compile("foo.bar[(1;2)]"));
+    println!("{:?}", syntax::compile("if 1 then 2"));
+    println!("{:?}", syntax::compile("()"));
+    println!("{:?}", syntax::compile("(1)"));
+    println!("{:?}", syntax::compile("(1; 2)"));
+    println!("{:?}", syntax::compile("foo(1, 2)"));
+    println!("{:?}", syntax::compile("posn { x=1, y=2 }"));
+    println!("{:?}", syntax::compile("foo := nil"));
+    println!("{:?}", syntax::compile("while 1 do if 2 then break"));
+    println!("{:?}", syntax::compile("let type a = int in 2 end"));
+    println!("{:?}", syntax::compile("let var a := 1 in a end"));
+    println!("{:?}", syntax::compile("let var a : int := 1 in a end"));
+    println!("{:?}", syntax::compile("let function foo(a:int) = () in a end"));
+    println!("{:?}", syntax::compile("let function foo(a:int) : int = 1 in a end"));
+    println!("{:?}", syntax::compile("foo #5# of 0"));
+    println!("{:?}", syntax::compile("-5"));
 }
