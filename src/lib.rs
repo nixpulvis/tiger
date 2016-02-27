1fa1b9b427d921a03e0202041e4fe034deaae052
Nathan Lilienthal <nathan@nixpulvis.com>
2016-02-26 23:50:03 -0500

    Add smoke test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
 extern crate tiger_syntax as syntax;
 
+use env::Env;
+use trans::Translate;
+
 pub mod env;
 pub mod ir;
 pub mod ty;
 pub mod trans;
+
+pub fn compile(source: &str) -> trans::Translation {
+    let tenv = Env::default();
+    let venv = Env::default();
+    let ast = syntax::parse(&source).unwrap();
+    ast.translate(&tenv, &venv)
+}
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,34 +1,20 @@
 extern crate tiger;
 extern crate tiger_syntax as syntax;
 
-use tiger::env::Env;
-use tiger::ty::Type;
-use tiger::trans::Translate;
-
-mod fixtures {
-    use std::io::prelude::*;
-    use std::path::Path;
-    use std::fs::File;
-    use syntax;
-    use syntax::ast::Expression;
-
-    pub fn parse<P: AsRef<Path>>(name: P) -> Box<Expression> {
-        let path = Path::new("fixtures").join(name);
-        let mut file = File::open(path)
-            .expect("could not open fixture");
-        let mut source = String::new();
-        file.read_to_string(&mut source)
-            .expect("error reading fixture");
-        syntax::parse(&source)
-            .expect("error parsing fixture")
-    }
-}
+use std::io::prelude::*;
+use std::fs::{self, File};
 
 #[test]
-fn test_translate() {
-    let tenv = Env::default();
-    let venv = Env::default();
-    let ast = fixtures::parse("merge.tig");
-    let trans = ast.translate(&tenv, &venv);
-    assert_eq!(Type::Unit, trans.ty);
+fn test_smoke() {
+    for entry in fs::read_dir("fixtures").unwrap() {
+        let entry = entry.unwrap();
+        if entry.file_name().to_str().unwrap().contains(".tig") {
+            let path = entry.path();
+            println!("Testing file: {}", path.to_str().unwrap());
+            let mut file = File::open(&path).unwrap();
+            let mut source = String::new();
+            file.read_to_string(&mut source).expect("error reading fixture");
+            tiger::compile(&source);
+        }
+    }
 }
