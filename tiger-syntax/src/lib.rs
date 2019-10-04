ae93c8993588d60436f879619e8e3734e4090cdd
Nathan Lilienthal <nathan@nixpulvis.com>
2019-10-03 23:10:21 -0400

    Get things compiling again, why not.

--- tiger-syntax/src/lib.rs
+++ tiger-syntax/src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lalrpop_util;
 extern crate unicode_xid;
 
 pub mod ast;
@@ -7,10 +9,10 @@ pub use ast::Ast;
 
 pub fn parse(source: &str) -> Result<Ast, String> {
     let lexer = tok::Lexer::new(source);
-    match tiger::parse_Expression(source, lexer) {
+    match tiger::ExpressionParser::new().parse(source, lexer) {
         Ok(ast) => Ok(ast),
         Err(e) => Err(format!("{:?}", e)),
     }
 }
 
-mod tiger;
+lalrpop_mod!(pub tiger);
